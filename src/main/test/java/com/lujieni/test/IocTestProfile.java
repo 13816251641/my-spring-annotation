50fdf91ea6cdb9d97e8d6fa15e875d42f28d9432
181268 <lujieni1017>
2020-07-09 14:09:43 +0800

    代码提交

--- src/main/test/java/com/lujieni/test/IocTestProfile.java
+++ src/main/test/java/com/lujieni/test/IocTestProfile.java
@@ -6,6 +6,7 @@ import com.lujieni.config.MainConfigOfPropertyValue;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
@@ -24,9 +25,9 @@ public class IocTestProfile {
     public void test01(){
         //1.创建一个applicationContext
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
-        Environment environment = applicationContext.getEnvironment();
+        ConfigurableEnvironment environment = applicationContext.getEnvironment();
         //2.设置一个需要激活的环境
-        environment.acceptsProfiles("dev","test");
+        environment.setActiveProfiles("prod");
         //3.注册主配置类
         applicationContext.register(MainConfigOfProfile.class);
         //4.启动刷新容器
